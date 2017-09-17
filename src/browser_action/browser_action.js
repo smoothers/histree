49dca74698a6e4b137af47a8a2e1d4796048283b
Elliot Plant <elliotaplant@gmail.com>
2017-09-17 11:51:32 -0700

    Add example data

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -3,26 +3,77 @@
 // d3.min (d3 visualization library)
 // visualization
 // * browser_action.js
-
-// This is the starting point for the 'main' action
-// We first get the active tab when the the browser_action button is clicked
-getActiveTab(activeTab => {
-  // Once we've recieved the tab, we can then ask the background task for the tab's tree
-  chrome.extension.sendMessage({
-      from: sender,
-      action: 'get-tree',
-      data: {
-        tab: activeTab
-      }
+const exampleData = {
+  url: 'http://www.example.com/moo/foo/bar',
+  title: 'Foos bars and the bar foods',
+  children: [
+    {
+      url: 'http://www.ixce.cods/mar/pars',
+      title: 'Ice DOgs 12098 12980 1928 ',
+      children: [
+        {
+          url: 'http://www.ixce.cods/mar/pars',
+          title: 'Ice DOgs 12098 12980 1928 ',
+          children: []
+        },
+        {
+          url: 'http://www.ixce.cods/mar/pars',
+          title: 'Ice DOgs 12098 12980 1928 ',
+          children: []
+        },
+      ]
     },
-    function(getTreeResponse) {
-      // Once we've recieved the tree from the background
-      // we set the root node's position
-      root = getTreeResponse.root;
-      root.x0 = height / 2;
-      root.y0 = 0;
+    {
+      url: 'http://www.ixce.cods/mar/pars',
+      title: 'Ice DOgs 12098 12980 1928 ',
+      children: [
+        {
+          url: 'http://www.ixce.cods/mar/pars',
+          title: 'Ice DOgs 12098 12980 1928 ',
+          children: []
+        },
+        {
+          url: 'http://www.ixce.cods/mar/pars',
+          title: 'Ice DOgs 12098 12980 1928 ',
+          children: []
+        },
+      ]
+    },
+  ]
+}
 
-      // Then we call the 'update' method to trigger the rest of the visualization
-      update(root);
-    });
-});
+// // This is the starting point for the 'main' action
+// // We first get the active tab when the the browser_action button is clicked
+// getActiveTab(activeTab => {
+//   // Once we've recieved the tab, we can then ask the background task for the tab's tree
+//   chrome.extension.sendMessage({
+//       from: sender,
+//       action: 'get-tree',
+//       data: {
+//         tab: activeTab
+//       }
+//     },
+//     function(getTreeResponse) {
+//       // Once we've recieved the tree from the background
+//       // we set the root node's position
+//       root = getTreeResponse.root;
+//       root.x0 = height / 2;
+//       root.y0 = 0;
+//
+//       // Then we call the 'update' method to trigger the rest of the visualization
+//       update(root);
+//     });
+// });
+
+function drawTree(tree) {
+  // Once we've recieved the tree from the background
+  // we set the root node's position
+  root = tree;
+  root.x0 = height / 2;
+  root.y0 = 0;
+
+  // Then we call the 'update' method to trigger the rest of the visualization
+  update(root);
+}
+
+drawTree(exampleData);
