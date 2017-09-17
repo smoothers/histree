b29d4c8009d3d50fe4cb08f4c731ae6bfa55762a
dank93 <dkurek93@gmail.com>
2017-09-16 17:37:42 -0700

    background responds to inject message

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,28 +1,10 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "page_loaded" ) {
 
+      console.log("brown cow");
 
-//example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   });
-console.log('mooooo');
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  console.log('tab', tab);
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {
-      "message": "clicked_browser_action"
-    });
-  });
-});
+    }
+  }
+);
