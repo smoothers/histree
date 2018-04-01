c99da21142f05fe5c4f1918138143f1f4cd03e7d
Elliot Plant <elliotaplant@gmail.com>
2018-04-01 11:01:58 -0700

    Use background process to listen for tab changes instead of injected script

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,23 +6,18 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // Handles requests from browser_action.js
   if (request.from === 'browser_action') {
     if (request.action === 'get-tree') {
-      console.log('sending', histreeStorage.getHistreeForTabId(request.data.tab.id));
       return sendResponse(histreeStorage.getHistreeForTabId(request.data.tab.id));
-    }
-  // Handles requests from the injected script
-  } else if (request.from === 'inject') {
-    if (request.action === 'insert-node') {
-      // If an insert node request comes in, insert that node ito the storage tree
-      histreeStorage.insertNodeIntoTreeByTabId({ url: sender.url, title: request.data.title },
-        sender.tab.id);
-
-      // Send an empty response to tell the page the insertion was complete
-      return sendResponse()
     }
   }
 });
 
-// Not sure what this is doing - can we remove it?
-chrome.history.onVisited.addListener(
-  (result) => {}
-);
+// Listens for changes to tabs to see when pages are loaded
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    histreeStorage.insertNodeIntoTreeByTabId({
+      url: tab.url,
+      title: tab.title,
+      favIconUrl: tab.favIconUrl
+    }, tab.id);
+  }
+});
--- src/inject/inject.js
+++
@@ -1,8 +0,0 @@
-// Tells the background to add this page as a node in this tab's Histree
-chrome.runtime.sendMessage({
-	from: 'inject',
-	action: 'insert-node',
-	data: {
-		title: document.title
-	}
-});
